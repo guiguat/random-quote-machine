5533c9131a8427b3af8d90192f94d70117095cf1
Guilherme Guatura <guaturadzn@gmail.com>
2020-07-19 23:21:22 -0300

    get quotes from api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
 function App() {
-
   const {color, author, text} = useSelector(state => state.data);
   const dispatch = useDispatch();
+
+  useEffect(()=>{
+    async function getQuotes(){
+      fetch("https://type.fit/api/quotes")
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        dispatch({type:"GET_QUOTES", payload: data})
+      });
+    }
+    try {
+      getQuotes()
+    } catch (error) {
+      alert(error)
+    }
+  },[dispatch]);
+
   const handleSubmit = () => {
     dispatch({type: "NEW_QUOTE"})
   }
+
   return (
     <div className="App" style={{backgroundColor: `${color}`}}>
       <div id="quote-box">
@@ -20,7 +38,10 @@ function App() {
           <span id="author" style={{color: `${color}`}}> - {author}</span>
         </div>
         <div className="buttons">
-          <a href="#" id="tweet-quote" style={{backgroundColor: `${color}`}} className="btn">Tweet Quote</a>
+          <a href={`https://twitter.com/intent/tweet?hashtags=quotes&text=${text} - ${author}`} 
+          id="tweet-quote" style={{backgroundColor: `${color}`}} className="btn">
+            <img src="https://img.icons8.com/metro/26/ffffff/twitter.png" alt="Tweet Quote"/>
+          </a>
           <button 
             id="new-quote" 
             style={{backgroundColor: `${color}`}} 
